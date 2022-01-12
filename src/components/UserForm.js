e4d10dbdad769f7bf22afe27d822326d654bbcb8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-12 14:42:20 -0800

    add first name and last name to form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,21 +2,26 @@ import React, { useState } from 'react';
 
 
 const UserForm = (props) => {
-    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { username, email, password };
+        const newUser = { firstName, lastName, email, password };
         console.log("Welcome", newUser);
     };
 
     return (
         <form onSubmit={createUser}>
             <div>
-                <label>Username: </label>
-                <input type="text" onChange={(e) => setUsername(e.target.value)} />
+                <label>First Name: </label>
+                <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div>
+                <label>Last Name: </label>
+                <input type="text" onChange={(e) => setLastName(e.target.value)} />
             </div>
             <div>
                 <label>Email Address: </label>
